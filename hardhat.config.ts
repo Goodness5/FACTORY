4cc45c48fcaa0b05b01211ef761e98043804e93a
superman <goodnesskolapo@gmail.com>
2023-02-11 17:33:46 +0100

    omo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
 
+dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+
+  networks: {
+    hardhat:{
+
+    },
+    goerli:{
+      url: process.env.GOERLI_RPC,
+      //@ts-ignore
+      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY2],
+    }
+  },
 };
 
 export default config;
